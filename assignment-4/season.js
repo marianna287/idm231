d55fd07a5722798d10b8f778db13f327b8238603
mariannalambros <mlamb287@gmail.com>
2022-03-11 15:01:13 -0500

    zodiacSign

--- assignment-4/season.js
+++ assignment-4/season.js
@@ -5,9 +5,9 @@ function season() {
     var season = ['Fall', 'Winter', 'Spring', 'Summer'];
 
   if ((month == 9 && day >=22) || (month == 10 && day <=31) || (month == 11 && day <=30) || (month == 12 && day <=21)) {
-      seasonTime = 0;
+    seasonTime = 0;
   } else if ((month == 1 && day <=31) || (month == 2 && day <=29) || (month == 3 && day <=21) || (month == 12 && day >=22)) {
-        seasonTime = 1;
+    seasonTime = 1;
   } else if ((month == 3 && day >=22) || (month == 4 && day <=30) || (month == 5 && day <=31) || (month == 6 && day <=21)) {
     seasonTime = 2;
   } else if ((month == 6 && day >=22) || (month == 7 && day <=31) || (month == 8 && day <=31) || (month == 9 && day <=21)) {
