7797f5a96a7292b3da1c061c52cc7f563e9923ea
mariannalambros <mlamb287@gmail.com>
2022-03-14 12:33:09 -0400

    non-broken image

--- final-beta/index.js
+++ final-beta/index.js
@@ -13,40 +13,40 @@ const findZodiac = (event) => {
 
 
     if ((month === 0 && day >= 20) || (month === 1 && day <= 18)) {
-        zodiacSign = "aquarius";
+        zodiacSign = "Aquarius";
         zodiacDate = "January 20th - February 18th";
       } else if ((month === 1 && day >=19) || (month === 2 && day <=20)) {
-        zodiacSign = "pisces";
+        zodiacSign = "Pisces";
         zodiacDate = "February 19th - March 20th";
       } else if ((month === 2 && day >=21) || (month === 3 && day <=19)) {
-        zodiacSign = "aries";
+        zodiacSign = "Aries";
         zodiacDate = "March 21 - April 19th";
       } else if ((month === 3 && day >=20) || (month === 4 && day <=20)) {
-        zodiacSign = "taurus";
+        zodiacSign = "Taurus";
         zodiacDate = "April 20th - May 20th";
       } else if ((month === 4 && day >=20) || (month === 5 && day <=20)) {
-        zodiacSign = "gemini";
+        zodiacSign = "Gemini";
         zodiacDate = "May 21 - June 20th";
       } else if ((month === 5 && day >=21) || (month === 6 && day <=22)) {
-        zodiacSign = "cancer";
+        zodiacSign = "Cancer";
         zodiacDate = "June 21 - July 22";
       } else if ((month === 6 && day >=23) || (month === 7 && day <=22)) {
-        zodiacSign = "leo";
+        zodiacSign = "Leo";
         zodiacDate = "July 23rd - August 22";
       } else if ((month === 7 && day >=23) || (month === 8 && day <=22)) {
-        zodiacSign = "virgo";
+        zodiacSign = "Virgo";
         zodiacDate = "August 23rd - September 22nd";
       } else if ((month === 8 && day >=23) || (month === 9 && day <=22)) {
-        zodiacSign = "libra";
+        zodiacSign = "Libra";
         zodiacDate = "September 23rd - October 22";
       } else if ((month === 9 && day >=23) || (month === 10 && day <=21)) {
-        zodiacSign = "scorpio";
+        zodiacSign = "Scorpio";
         zodiacDate = "October 23rd - November 21";
       } else if ((month === 10 && day >=22) || (month === 11 && day <=21)) {
-        zodiacSign = "sagittarius";
+        zodiacSign = "Sagittarius";
         zodiacDate = "November 22 - December 21";
       } else if ((month === 11 && day >=22) || (month === 0 && day <=19)) {
-        zodiacSign = "capricorn";
+        zodiacSign = "Capricorn";
         zodiacDate = "December 22nd - January 19th";
     }
   
@@ -54,7 +54,7 @@ const findZodiac = (event) => {
     document.getElementById("zodiac-date").innerHTML = zodiacDate;
 
     const imageElement = document.querySelector(".zodiacImage");
-    imageElement.src = "images/{zodiacSign}.png";
+    imageElement.src = `images/${zodiacSign}.png`;
     imageElement.alt = zodiacSign;
 
 
