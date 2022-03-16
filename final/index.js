161f62f630041b1fed5976ebc12ee05cdebdfdf0
mariannalambros <mlamb287@gmail.com>
2022-03-16 16:31:33 -0400

    help pop up

--- final/index.js
+++ final/index.js
@@ -4,73 +4,73 @@ const orderedZodiacSigns = {
   "aquarius": {
     sign: "Aquarius",
     date: "January 20th - February 18th",
-    audio: new Audio('sounds/examples_projects_05-soundboard_src_sounds_bullet.mp3')
+    audio: new Audio('sounds/water2.mp3')
   },
   
   "pisces": {
     sign: "Pisces",
     date: "February 19th - March 20th",
-    audio: new Audio('https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3')
+    audio: new Audio('sounds/bubbles.mp3')
   },
   
   "aries": {
     sign: "Aries",
     date: "March 21 - April 19th",
-    audio: new Audio('https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3')
+    audio: new Audio('sounds/fire2.mp3')
   },
   
   "taurus": {
     sign: "Taurus",
     date: "April 20th - May 20th",
-    audio: new Audio('https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3')
+    audio: new Audio('sounds/bull2.mp3')
   },
 
   "gemini": {
     sign: "Gemini",
     date: "May 21 - June 20th",
-    audio: new Audio('https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3')
+    audio: new Audio('sounds/wind2.mp3')
   },
 
   "cancer": {
     sign: "Cancer",
     date: "June 21 - July 22",
-    audio: new Audio('https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3')
+    audio: new Audio('sounds/crying.mp3')
   },
 
   "leo": {
     sign: "Leo",
     date: "July 23rd - August 22",
-    audio: new Audio('https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3')
+    audio: new Audio('sounds/lion2.mp3')
   },
 
   "virgo": {
     sign: "Virgo",
     date: "August 23rd - September 22nd",
-    audio: new Audio('https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3')
+    audio: new Audio('sounds/heavens2.mp3')
   },
 
   "libra": {
     sign: "Libra",
     date: "September 23rd - October 22",
-    audio: new Audio('https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3')
+    audio: new Audio('sounds/chimes2.mp3')
   },
 
   "scorpio": {
     sign: "Scorpio",
     date: "October 23rd - November 21",
-    audio: new Audio('https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3')
+    audio: new Audio('sounds/waterfall.mp3')
   },
 
   "sagittarius": {
     sign: "Sagittarius",
     date: "November 22 - December 21",
-    audio: new Audio('https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3')
+    audio: new Audio('sounds/arrow.mp3')
   },
 
   "capricorn": {
     sign: "Capricorn",
     date: "December 22nd - January 19th",
-    audio: new Audio('https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3')
+    audio: new Audio('sounds/goats2.mp3')
   },
 }
 
@@ -136,6 +136,40 @@ for(let i = 0; i < buttons.length; i++){
   })
 }
 
+const openModalButtons = document.querySelectorAll('[data-modal-target')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
 
 
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
 
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
